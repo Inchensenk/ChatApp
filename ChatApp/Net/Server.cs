233afaad75292570e8519c20946e791676c59e51
sofoyan gevork <aab81@bk.ru>
2022-10-06 17:51:45 +0300

    Добавьте файлы проекта.

---
+++ ChatApp/Net/Server.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.Net
+{
+    internal class Server
+    {
+        TcpClient _client;
+        public Server()
+        {
+            _client = new();
+        }
+
+        public void ConnectToServer()
+        {
+            if(!_client.Connected)
+            {
+                //Локальный IP адрес и порт
+                _client.Connect("10.61.140.37", 7891);
+            }
+        }
+    }
+}
