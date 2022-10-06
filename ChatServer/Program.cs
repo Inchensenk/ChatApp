fdc57af36e4f578f7f57db77611dc8633e07fb0e
sofoyan gevork <aab81@bk.ru>
2022-10-06 18:06:33 +0300

    первый запуск

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        static TcpListener _listener;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _listener = new TcpListener(IPAddress.Parse("10.61.140.37"), 7891);
+            _listener.Start();
+
+            var client = _listener.AcceptTcpClient();
+            Console.WriteLine("Client has connected!");
         }
     }
 }
