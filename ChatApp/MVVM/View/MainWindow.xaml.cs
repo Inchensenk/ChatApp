c7fc4c0dafe47c939435f6093b59f20693bc2fe8
Inchensenk <aab8117@gmail.com>
2022-10-13 11:09:58 +0300

    @UserName WaterMark in connect TextBox

--- ChatApp/MVVM/View/MainWindow.xaml.cs
+++ ChatApp/MVVM/View/MainWindow.xaml.cs
@@ -25,14 +25,5 @@ namespace ChatApp
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
-
-        }
     }
 }
